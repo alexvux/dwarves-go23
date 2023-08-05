2c795b351e7f770ce1e437bdf0f66e03882a3fda
E590\Admin <binhvd219@gmail.com>
2023-08-06 00:35:54 +0700

    add more unit test for ex1

--- ex1/main_test.go
+++ ex1/main_test.go
@@ -1,6 +1,42 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	testCases := []struct {
+		name   string
+		args   []string
+		output string
+	}{
+		{"not enough args", []string{"Binh", "TH"}, "Need at least 3 arguments passed, has only: 2"},
+		{"invalid country code", []string{"Binh", "Vu", "CA"}, "Invalid country code: CA\n"},
+		{"valid VN name", []string{"John", "Smith", "VN"}, "Output: Smith John"},
+		{"valid US name", []string{"Emily", "Rose", "Watson", "US"}, "Output: Emily Watson Rose"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// redirect stdout to another memory for capturing the output
+			oldStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+			// run the main func with test args
+			os.Args = append([]string{"main.go"}, tc.args...)
+			main()
+			// restore stdout and read the captured output
+			os.Stdout = oldStdout
+			output := make([]byte, 1024)
+			n, _ := r.Read(output)
+
+			if string(output[:n]) != tc.output {
+				t.Errorf("Test %q: got %q, want %q", tc.name, output[:n], tc.output)
+			}
+		})
+	}
+}
 
 func Test_isValidCountryCode(t *testing.T) {
 	testCases := []struct {
@@ -9,12 +45,12 @@ func Test_isValidCountryCode(t *testing.T) {
 		expected bool
 	}{
 		{
-			name:     "Valid country code",
+			name:     "valid country code",
 			input:    "VN",
 			expected: true,
 		},
 		{
-			name:     "Invalid country code",
+			name:     "invalid country code",
 			input:    "CA",
 			expected: false,
 		},
